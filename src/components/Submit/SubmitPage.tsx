c0daa02a2f84d2221c6cecfb9734164ba7604a71
Greyson Knippel <gaknippel@hotmail.com>
2025-11-18 22:31:32 -0800

    tried to make the submit page look nicer (will add stuff on right side)

--- src/components/Submit/SubmitPage.tsx
+++ src/components/Submit/SubmitPage.tsx
@@ -1,7 +1,6 @@
 import './SubmitPage.css';
 import SplitText from '../ReactBitsStuff/SplitText';
 import React, { useState } from 'react';
-
 const Submit = () => {
 
   const[status, setStatus]  = useState('');
@@ -42,37 +41,32 @@ const Submit = () => {
     <div>
       
       <SplitText className='submit-welcome'
-        text="Submit a Clip"
+        text="Submit a Clip!"
         delay={15}
       />
-
-      <div className="submit-body">
-      <form onSubmit={handleSubmit}>
-        <div className="form-body">
-          <label htmlFor="name">Your Name (optional)</label>
-          <input type="text" id="name" name="name" />
-        </div>
-        <div className="form-group">
-          <label htmlFor="clipUrl">Clip URL (Twitch, YouTube)</label>
-          <input type="url" id="clipUrl" name="clipUrl" required />
-        </div>
-        <div className="form-group">
-          <label htmlFor="message">Message (Optional)</label>
-          <textarea id="message" name="message" rows={2} />
-        </div>
-        
-        <button 
-          type="submit" 
-          style={{ cursor: 'pointer', padding: '10px 20px' }}
-        >
-          Submit Clip
-        </button>
-        
-        {status && <p className="status-message">{status}</p>}
-      </form>
-    </div>
-      
-    </div>
+        <div className="submit-left">
+          <form onSubmit={handleSubmit}>
+            <div className="form-body">
+              <label htmlFor="name">Your Name (optional)</label>
+              <input type="text" id="name" name="name" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="clipUrl">Clip URL (Twitch, YouTube)</label>
+              <input type="url" id="clipUrl" name="clipUrl" required />
+            </div>
+            <div className="form-group">
+              <label htmlFor="message">Message (Optional)</label>
+              <textarea id="message" name="message" rows={2} />
+            </div>
+            
+            <button type="submit">
+              Submit Clip
+            </button>
+            
+            {status && <p className="status-message">{status}</p>}
+          </form>
+        </div>   
+  </div>
 
     
   );
