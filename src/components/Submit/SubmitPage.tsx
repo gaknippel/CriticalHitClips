5e0c57e7e2c75a080c4c692162212cf0562f0edd
Greyson Knippel <gaknippel@hotmail.com>
2025-11-21 15:00:33 -0800

    trying a video for the submit page (experimental)

--- src/components/Submit/SubmitPage.tsx
+++ src/components/Submit/SubmitPage.tsx
@@ -1,7 +1,7 @@
 import './SubmitPage.css';
 import SplitText from '../ReactBitsStuff/SplitText';
 import React, { useState } from 'react';
-import FadeContent from '../ReactBitsStuff/FadeContent/FadeContent';
+import submitPageClip from '../../assets/submitPageClip.mp4';
 
 const Submit = () => {
 
@@ -46,32 +46,43 @@ const Submit = () => {
         text="Submit a Clip!"
         delay={15}
       />
+      <video 
+        autoPlay 
+        loop 
+        muted 
+        playsInline 
+        className="video-background"
+      >
 
-      <FadeContent blur={false} duration={1000} easing="ease-out" initialOpacity={0}> 
-        <div className="submit-left">
-          <form onSubmit={handleSubmit}>
-            <div className="form-body">
-              <label htmlFor="name">Name (optional)</label>
-              <input type="text" id="name" name="name" />
-            </div>
-            <div className="form-group">
-              <label htmlFor="clipUrl">Clip URL (Twitch, YouTube)</label>
-              <input type="url" id="clipURl" name="clipURL" required />
-            </div>
-            <div className="form-group">
-              <label htmlFor="message">Message (optional)</label>
-              <textarea id="message" name="message" rows={2} />
-            </div>
-            
-            <button type="submit">
-              Submit Clip
-            </button>
-            
-            {status && <p className="status-message">{status}</p>}
-          </form>
-        </div>  
-        </FadeContent> 
-  </div>
+        
+        {<source src={submitPageClip} type="video/mp4" />}
+      </video>
+
+      <div className="video-overlay"></div>
+      
+          <div className="submit-left">
+            <form onSubmit={handleSubmit}>
+              <div className="form-body">
+                <label htmlFor="name">Name (optional)</label>
+                <input type="text" id="name" name="name" />
+              </div>
+              <div className="form-group">
+                <label htmlFor="clipUrl">Clip URL (Twitch, YouTube)</label>
+                <input type="url" id="clipURl" name="clipURL" required />
+              </div>
+              <div className="form-group">
+                <label htmlFor="message">Message (optional)</label>
+                <textarea id="message" name="message" rows={2} />
+              </div>
+              
+              <button type="submit">
+                Submit Clip
+              </button>
+              
+              {status && <p className="status-message">{status}</p>}
+            </form>
+          </div>
+    </div>
 
     
   );
