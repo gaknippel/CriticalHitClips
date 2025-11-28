e100194da5040da51ad8e3208eefc311475cb879
Greyson Knippel <gaknippel@hotmail.com>
2025-11-27 22:52:42 -0800

    added privacy page (not finished)

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
         <footer className="app-footer">
             <div className="footer-links">
                 <a href="https://github.com/gaknippel" target="_blank" rel="noopener noreferrer">GitHub</a>
-                <a href="https://youtube.com/@critterfarts" target="_blank" rel="noopener noreferrer">YouTube</a>
+                <a href="https://www.youtube.com/@CriticalHitClipstf2" target="_blank" rel="noopener noreferrer">YouTube</a>
                 <a href="/privacy-policy">Privacy Policy</a>
             </div>
             <div className="footer-copyright">
--- src/components/Privacy/PrivacyPage.tsx
+++ src/components/Privacy/PrivacyPage.tsx
@@ -1,12 +1,46 @@
 import './PrivacyPage.css';
+import SplitText from '../../components/ReactBitsStuff/SplitText'; 
+import steamFacepalm from '../../assets/steamFacepalm.gif';
+import FadeContent from '../../components/ReactBitsStuff/FadeContent/FadeContent';
 
-const HomePage = () => {
+const handleAnimationComplete = () => {
+  console.log('All letters have animated!');
+};
+
+const PrivacyPage = () => {
   return (
-    <div>
+    <div className='privacy-page-wrapper'>
+      <div className="support-welcome">
+        <SplitText
+          text="Privacy Policy"
+          delay={15}
+        />
+      </div>
 
-        
+      <FadeContent blur={false} duration={500} easing="ease-out" initialOpacity={0}>
+       <div className="privacy-content-box">
+            
+            <div className='privacy-body'>
+              <SplitText
+                text="problems? contact gaknippel@hotmail.com"
+                delay={15}
+                duration={0.6}
+                ease="power3.out"
+                splitType="chars"
+                from={{ opacity: 0, y: 40 }}
+                to={{ opacity: 1, y: 0 }}
+                threshold={0.1}
+                rootMargin="-100px"
+                textAlign="center"
+                onLetterAnimationComplete={handleAnimationComplete}
+              />
+            </div>
+
+
+       </div>
+      </FadeContent>
     </div>
   );
 };
 
-export default HomePage;
+export default PrivacyPage;
