88cf161ede6ef119fe4c5f06ed271f80645af746
Greyson Knippel <gaknippel@hotmail.com>
2026-01-10 16:22:10 -0800

    added embed for videos in home page

--- src/components/Home/HomePage.tsx
+++ src/components/Home/HomePage.tsx
@@ -1,6 +1,16 @@
 import SplitText from "../ReactBitsStuff/SplitText";
 import "./HomePage.css";
 
+// To add more videos, add a new object to this array
+const videos = [
+  {
+    id: "dQw4w9WgXcQ", // Replace with your YouTube video ID
+    title: "Check out the latest video!",
+    description:
+      "This is a placeholder video. You can replace the ID with your own YouTube video ID.",
+  },
+];
+
 const HomePage = () => {
   return (
     <div className="home-page-wrapper">
@@ -9,6 +19,32 @@ const HomePage = () => {
         text="Welcome to CriticalHitClips!"
         delay={15}
       />
+      <div className="video-section">
+        {videos.map((video) => (
+          <div key={video.id} className="video-container">
+            <h2>{video.title}</h2>
+            <div className="video-embed">
+              <iframe 
+              width="2403" 
+              height="1155" 
+              src="https://www.youtube.com/embed/74h3OdWCyl4" 
+              title="TF2 MOST VIEWED Clips of The Month! | criticalhitclips#1" 
+              frameBorder="0" 
+              allow="accelerometer; 
+              autoplay; 
+              clipboard-write; 
+              encrypted-media; 
+              gyroscope; 
+              picture-in-picture; 
+              web-share" 
+              referrerPolicy="strict-origin-when-cross-origin" 
+              allowFullScreen>
+                
+              </iframe>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
